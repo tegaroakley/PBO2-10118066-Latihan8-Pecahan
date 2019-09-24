9025fedb5bfc42542feebcaff0a97c8aacf4088d
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 15:55:10 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan8/pecahan/PBO210118066Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Tegar Lucky'Q Oakley 
+ * NIM   : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk Menampilkan Pecahan
+ */
+public class PBO210118066Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+        
+    }
+    
+}
